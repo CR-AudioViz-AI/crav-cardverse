3d6c67e2fd476b7e2b6b5a032d540c7e69bb17f4
Old <225056366+roy-henderson@users.noreply.github.com>
2025-12-12 16:52:15 -0500

    rebrand: Update JavariWrapper - CardVerse to CravCards

--- components/JavariWrapper.tsx
+++ components/JavariWrapper.tsx
@@ -11,14 +11,14 @@ interface JavariWrapperProps {
   sourceApp?: string;
 }
 
-export default function JavariWrapper({ sourceApp = 'cardverse' }: JavariWrapperProps) {
+export default function JavariWrapper({ sourceApp = 'cravcards' }: JavariWrapperProps) {
   return (
     <JavariWidget 
       sourceApp={sourceApp}
       position="bottom-right"
       enableTickets={true}
       enableEnhancements={true}
-      context="CardVerse digital trading card platform"
+      context="CravCards - Premium trading card collection platform for Pokemon, MTG, Sports Cards, and more"
     />
   );
 }
